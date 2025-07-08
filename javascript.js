1afffb13a1ba3fb087228b6e83550713ffd51ac9
aravalaharshavardhan <aravalaharsha@gmail.com>
2025-07-08 12:53:38 +0530

    Tried to write logic for a round.

--- javascript.js
+++ javascript.js
@@ -14,4 +14,33 @@ function getHumanChoice(){
     let choice = prompt("Rock, Paper or Scissors:");
     console.log(choice);
 }
+function playRound(computerchoice,humanchoice){
+    if(computerchoice == humanchoice){
+        console.log("It's a draw!");
+        humanscore++;
+        computerscore++;
+    }
+    else if (computerchoice == "Rock" && humanchoice == "Scissors"){
+        console.log("Rock beats scissors!");
+        computerscore++;
+    }
+    else if(computerchoice=="Scissors" && humanchoice == "Paper"){
+        console.log("Scissors cuts paper");
+        computerscore++;
+    }
+    else if (computerchoice == "Paper" && humanchoice == "Rock"){
+        console.log("Paper envelops rock.");
+        computerscore++;
+    }
+    else{
+        console.log("You win.");
+        humanscore++;
+    }
+
+}
+let humanscore = 0;
+let computerscore = 0;
 getComputerChoice();
+getHumanChoice();
+const computerchoice = getComputerChoice();
+const humanchoice = getHumanChoice();
