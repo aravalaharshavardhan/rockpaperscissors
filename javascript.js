3f2f998f0ec6068148532f957932a7736ed1f5e8
aravalaharshavardhan <aravalaharsha@gmail.com>
2025-07-08 12:36:04 +0530

    Got both choices

--- javascript.js
+++ javascript.js
@@ -1,2 +1,17 @@
-
-console.log("Hello World");
+function getComputerChoice(){
+    let random = Math.random();
+    if(random < 0.5){
+        console.log("Rock");
+    }
+    else if(random > 0.5){
+        console.log("Paper");
+    }
+    else{
+        console.log("Scissors");
+    }
+}
+function getHumanChoice(){
+    let choice = prompt("Rock, Paper or Scissors:");
+    console.log(choice);
+}
+getComputerChoice();
